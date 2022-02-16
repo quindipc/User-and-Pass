7f0fe88a0e603f3ba19f415461ca700fc1794333
Chelsea Quindipan <quindipc@gmail.com>
2022-02-16 14:08:05 -0500

    Adding functions

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-// Single element 
-const form = document.getElementById('my-form');
-const form = document.querySelector('my-form');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
 
-// Multiple element
+// Event Listeners
+myForm. addEventListener('submit', onSubmit);
+
+
+
+// Functions
+function onSubmit(e) {
+    e.preventDefault();
+
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = "Please enter all fields";
+
+       // setTimeout(() => msg.remove(), 3000);
+    } else {
+       const li = document.createElement('li');
+       li.appendChild(document.createTextNode('${nameInput.value} : ${emailInput.value}'));
+
+       userList.appendChild(li);
+
+       //Clearing fields 
+       nameInput.value = '';
+       emailInput.value = '';
+    }
+}
+
+// This only saves on the UI, this doesn't save to a database since there isn't a database yet. 
